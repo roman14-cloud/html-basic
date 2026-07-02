07af697f02be03234cd45ef79f8dc87e9523a4e4
roman14-cloud <romanian241@gmail.com>
2026-07-02 10:49:18 +0300

    javascript

---
+++ assets/js/opera.js
@@ -0,0 +1,12 @@
+// Function to display a message
+        function displaymessage() {
+            alert('Welcome to JavaScript!');
+        }
+
+
+    function safari(){
+        alert("you doubled click your button")
+    }
+    function changecolor(){
+        document.getElementById("mybtn").style.backgroundColor = "yellow";
+    }
